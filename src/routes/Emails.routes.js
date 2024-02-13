0e3cc0bcd9152a3fec6b07e65e0a5e411abeffbf
SamuelGonza <samuel@tecnotics.com>
2024-02-13 15:41:59 -0500

    multer

--- src/routes/Emails.routes.js
+++ src/routes/Emails.routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { enviarCita } from "../controllers/Email.controller.js";
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
 const router = Router();
 
 
-router.post("/citas/send", enviarCita)
+router.post("/citas/send", upload.array("archivos", 3), enviarCita);
 
 export default router;
